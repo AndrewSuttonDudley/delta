b8491db31eea59ec5f1c75927c87ed7e96c7d7bc
Andrew Dudley <andrew.sutton.dudley@gmail.com>
2023-06-19 12:54:27 -0400

    Created project

---
+++ src/test/java/com/crscreditapi/delta/TestDeltaApplication.java
@@ -0,0 +1,22 @@
+package com.crscreditapi.delta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestDeltaApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:latest");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(DeltaApplication::main).with(TestDeltaApplication.class).run(args);
+    }
+
+}
